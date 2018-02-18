348c9291fc62b2751f8cf89a5f48d2da2970c6fb
Fadi Yousef <Fadi Yousef@DESKTOP-I06DSB7>
2018-02-18 17:46:36 +0200

    Small Updates

--- WebContent/js/EbooksController.js
+++ WebContent/js/EbooksController.js
@@ -35,4 +35,25 @@ angular.module('app',[])
 			localStorage.setItem('purchaseBook', JSON.stringify(book));
 			window.location="Purchase.html";
 		}
+		
+		$scope.CheckIfBookPurchased = function(book){
+			var books = JSON.parse(localStorage.getItem('loginResponse')).books;
+			for(var i=0 ; i<books.length ; i++){
+				if(books[i].Name == book.Name){
+					document.getElementById("PurchasedButton").disabled = true;
+					return "Already Purchased";
+				}
+			}
+			return "Purchase";
+		}
+		
+		$scope.CheckIfBookPurchasedIDs = function(book){
+			var books = JSON.parse(localStorage.getItem('loginResponse')).books;
+			for(var i=0 ; i<books.length ; i++){
+				if(books[i].Name == book.Name){
+					return "Purchased";
+				}
+			}
+			return "Purchase";
+		}
 	}]);
